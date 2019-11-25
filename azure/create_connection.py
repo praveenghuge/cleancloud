d130cbb9b95a7742369a882524153776ecb3233e
praveenghuge <33432891+praveenghuge@users.noreply.github.com>
2019-11-26 00:30:33 +0530

    Update create_connection.py

--- azure/create_connection.py
+++ azure/create_connection.py
@@ -1,9 +1,9 @@
 from azure.common.credentials import ServicePrincipalCredentials
 
-tenant_id = "eff7f985-dc58-4935-a906-050609be85c3"
-client_secret = "V+obVteoP+Y738u9bcOb7XP60ELdB2f15j524sM3MQA="
-subscriptionid = "64caccf3-b508-41e7-92ed-d7ed95b32621"
-client_id = "541ac203-1433-4eef-b965-54bccc293f8e"
+tenant_id = ""
+client_secret = ""
+subscriptionid = ""
+client_id = ""
 
 def get_connection():
     subscription_id = subscriptionid
